54bb7419da433237d223311d823f82e2481fc15f
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-07-13 18:36:41 -0400

    Adding comments

--- markdown.py
+++ markdown.py
@@ -6,8 +6,8 @@ from urllib import request
 
 
 class Header:
-    def __init__(self, text: str, level: int) -> None:
-        self.text: str = text
+    def __init__(self, text: Text, level: int) -> None:
+        self.text: Text = text
         self.level: int = level
 
     def __str__(self) -> str:
@@ -23,7 +23,7 @@ class Header:
 
 
 class OrderedList:
-    def __init__(self, items: Iterable) -> None:
+    def __init__(self, items: Iterable[Text]) -> None:
         self.items: Iterable = items
 
     def __str__(self) -> str:
@@ -31,9 +31,9 @@ class OrderedList:
 
 
 class Link:
-    def __init__(self, text: str, url: str) -> None:
-        self.text = text
-        self.url = url
+    def __init__(self, text: Text, url: str) -> None:
+        self.text: Text = text
+        self.url: str = url
 
     def __str__(self) -> str:
         return f"[{self.text}]({self.url})"
@@ -56,7 +56,7 @@ class Link:
 
 
 class Table:
-    def __init__(self, header: Iterable, body: Iterable[Iterable], footer: Iterable) -> None:
+    def __init__(self, header: Iterable[Text], body: Iterable[Iterable[Text]], footer: Iterable[Text]) -> None:
         self.header = header
         self.body = body
         self.footer = footer
@@ -74,18 +74,23 @@ class Table:
 
 
 class Text:
-    def __init__(self) -> None: 
-        self.content = list()
+    """
+    The basic unit of text in markdown. All components which contain
+    text are built using this class instead of strings directly. That
+    way, those elements capture all styling information. 
+    """
+
+    def __init__(self, text, style=None) -> None:
+        self.text = text
+        self.style = style
 
     def __str__(self) -> str:
-        return " ".join(self.content)        
+        if self.style == "bold":
+            text = f"**{self.text}**"
+        elif self.style == "italics":
+            text = f"*{self.text}*"
+        return text
 
-    def add(self, text, style=None):
-        if style == "bold":
-            text = f"**{text}**"
-        elif style == "italics":
-            text = f"*{text}*"
-        self.content.add(text)
 
 class Document:
 
@@ -98,10 +103,24 @@ class Document:
         return f"{self.name}\n{self._build_page()}"
 
     def add_element(self, element):
+        """
+        A generic function for appending elements to the document. 
+        Use this function when you want a little more control over
+        what the output looks like. 
+
+        :param element: a markdown object (e.g., Table, Header, etc.)
+        """
         self.contents.append(element)
 
-    def add_header(self, text, level):
-        self.contents.append(Header(text, level))
+    def add_header(self, text: str, level: int = 1):
+        """
+        Adds a header to the document.
+
+        :param text: the text for the header
+        :param level: the level of the header from 1 to 6
+        """
+        assert 1 <= level <= 6
+        self.contents.append(Header(Text(text), level))
 
     def add_text(self, text: str):
         self.contents.append(Text(text))

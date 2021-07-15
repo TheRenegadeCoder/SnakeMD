ce1d769413df4591649b5cc5cddf607aa2a207b4
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-07-15 14:37:13 -0400

    Updated project description

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="1.0.0",
     author="The Renegade Coder",
     author_email="jeremy.grifski@therenegadecoder.com",
-    description="A markdown generation tool written in Python.",
+    description="A markdown generation library for Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/TheRenegadeCoder/pymd",

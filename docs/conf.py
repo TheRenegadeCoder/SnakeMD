229e414fcf4a20489248e691016921176eed69d8
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-07-15 16:09:57 -0400

    Fixed version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, The Renegade Coder'
 author = 'The Renegade Coder'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------

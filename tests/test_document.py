f10ae23908d7ad8f64e11f5afb74350ce63b74e3
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-07-16 17:11:30 -0400

    Added some extra doc tests

--- tests/test_document.py
+++ tests/test_document.py
@@ -14,3 +14,9 @@ def test_document_add_paragraph():
     doc = Document("Test")
     doc.add_paragraph("Test Document")
     assert str(doc) == "Test Document"
+
+def test_document_add_header_and_paragraph():
+    doc = Document("Test")
+    doc.add_header("Test Document")
+    doc.add_paragraph("This is a test document.")
+    assert str(doc) == "# Test Document\n\nThis is a test document."

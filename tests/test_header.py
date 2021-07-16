07e4bb70a97bb5e00c3d85511eb47dcb3ce3c39d
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-07-16 16:11:18 -0400

    Completed header testing

--- tests/test_header.py
+++ tests/test_header.py
@@ -12,3 +12,39 @@ def test_header_str_level_one():
 def test_header_str_level_two():
     header = Header("Example Header", 2)
     assert str(header) == "## Example Header"
+
+def test_header_str_level_three():
+    header = Header("Example Header", 3)
+    assert str(header) == "### Example Header"
+
+def test_header_str_level_four():
+    header = Header("Example Header", 4)
+    assert str(header) == "#### Example Header"
+
+def test_header_str_level_five():
+    header = Header("Example Header", 5)
+    assert str(header) == "##### Example Header"
+
+def test_header_str_level_six():
+    header = Header("Example Header", 6)
+    assert str(header) == "###### Example Header"
+
+def test_header_promote():
+    header = Header("Example Header", 2)
+    header.promote()
+    assert str(header) == "# Example Header"
+
+def test_header_promote_max():
+    header = Header("Example Header", 1)
+    header.promote()
+    assert str(header) == "# Example Header"
+
+def test_header_demote():
+    header = Header("Example Header", 2)
+    header.demote()
+    assert str(header) == "### Example Header"
+
+def test_header_demote_min():
+    header = Header("Example Header", 6)
+    header.demote()
+    assert str(header) == "###### Example Header"

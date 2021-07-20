27849425e8a1aa55b2fecef732c5ff4e88b8ce5e
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-07-20 00:06:41 -0400

    Writing more tests

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,6 +1,14 @@
 from snake.md import Table
 
 
-def test_table_one():
+def test_table_one_col():
     table = Table(["Age"], [["37"]])
     assert str(table) == "| Age |\n| --- |\n| 37  |"
+
+def test_table_one_col_wide_body():
+    table = Table(["Age"], [["2337"]])
+    assert str(table) == "| Age  |\n| ---- |\n| 2337 |"
+
+def test_table_two_col():
+    table = Table(["Age", "Color"], [["37", "Blue"]])
+    assert str(table) == "| Age | Color |\n| --- | ----- |\n| 37  | Blue  |"

c40f3be293b667f0713c72308d850e996d961f1c
jrg94 <jeremy.grifski@gmail.com>
2023-05-02 18:37:00 -0400

    Sorted imports

--- snakemd/templates.py
+++ snakemd/templates.py
@@ -7,8 +7,7 @@ from __future__ import annotations
 
 import logging
 
-
-from .elements import Element, Heading, Inline, MDList, Block
+from .elements import Block, Element, Heading, Inline, MDList
 
 logger = logging.getLogger(__name__)
 

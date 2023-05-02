7b6d5462156b15a0f3581056d5c001db2a089ba9
jrg94 <jeremy.grifski@gmail.com>
2023-05-02 19:00:12 -0400

    Reformatted README

--- readme.py
+++ readme.py
@@ -105,12 +105,13 @@ def _section(doc: Document, title: str, desc: str, func: Callable, level: int = 
     doc.add_block(Paragraph([Inline("Rendered Result", italics=True)]))
     func(doc)
     doc.add_block(Paragraph([Inline("Markdown Source", italics=True)]))
-    block: Block = doc._elements[-2]
+    elements = doc.get_elements()
+    block: Block = elements[-2]
     doc.add_block(
         Code(block if isinstance(block, Code) else str(block), lang="markdown")
     )
-    doc._elements.insert(-3, doc._elements.pop())
-    doc._elements.insert(-3, doc._elements.pop())
+    elements.insert(-3, elements.pop())
+    elements.insert(-3, elements.pop())
 
 
 def main() -> None:

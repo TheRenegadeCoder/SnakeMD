3feac32bbc9260dc01b75fb9a14e705355157296
jrg94 <jeremy.grifski@gmail.com>
2023-05-03 14:53:30 -0400

    Added docs to tests

--- tests/templates/test_csv_table.py
+++ tests/templates/test_csv_table.py
@@ -3,15 +3,25 @@ from snakemd.templates import CSVTable
 
 
 def test_csv_table_type_error_none():
+    """
+    Verifies that None properly throws a TypeError.
+    """
     with pytest.raises(TypeError):
         CSVTable(None)
 
 
 def test_csv_table_file_not_found_error_empty():
+    """
+    Verifies that an empty string properly throws FileNotFoundError.
+    """
     with pytest.raises(FileNotFoundError):
         CSVTable("")
         
 def test_csv_table_sample_file():
+    """
+    Verifies that a sample CSV is properly converted into
+    a Markdown table.
+    """
     table = CSVTable("tests/resources/python-support.csv")
     table_lines = str(table).splitlines(keepends=True)
     with open("tests/resources/python-support.md") as md:

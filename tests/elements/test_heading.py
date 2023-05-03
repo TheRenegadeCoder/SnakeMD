4f8d7b2e73c681d15dc3d0535e1830b281f68ba1
jrg94 <jeremy.grifski@gmail.com>
2023-05-03 13:54:50 -0400

    Expanded testing and docs for heading code

--- tests/elements/test_heading.py
+++ tests/elements/test_heading.py
@@ -113,7 +113,7 @@ def test_heading_inline_bold_level_one():
 def test_heading_str_level_two():
     """
     Verifies that a Heading object properly 
-    initializes with as a level 2 heading. Also 
+    initializes as a level 2 heading. Also 
     verifies that the repr string is properly 
     formatted and that the markdown itself is 
     properly rendered.
@@ -139,7 +139,7 @@ def test_heading_str_level_two():
 def test_heading_str_level_three():
     """
     Verifies that a Heading object properly 
-    initializes with as a level 3 heading. Also 
+    initializes as a level 3 heading. Also 
     verifies that the repr string is properly 
     formatted and that the markdown itself is 
     properly rendered.
@@ -165,7 +165,7 @@ def test_heading_str_level_three():
 def test_heading_str_level_four():
     """
     Verifies that a Heading object properly 
-    initializes with as a level 4 heading. Also 
+    initializes as a level 4 heading. Also 
     verifies that the repr string is properly 
     formatted and that the markdown itself is 
     properly rendered.
@@ -191,7 +191,7 @@ def test_heading_str_level_four():
 def test_heading_str_level_five():
     """
     Verifies that a Heading object properly 
-    initializes with as a level 5 heading. Also 
+    initializes as a level 5 heading. Also 
     verifies that the repr string is properly 
     formatted and that the markdown itself is 
     properly rendered.
@@ -217,7 +217,7 @@ def test_heading_str_level_five():
 def test_heading_str_level_six():
     """
     Verifies that a Heading object properly 
-    initializes with as a level 6 heading. Also 
+    initializes as a level 6 heading. Also 
     verifies that the repr string is properly 
     formatted and that the markdown itself is 
     properly rendered.
@@ -238,14 +238,97 @@ def test_heading_str_level_six():
         r", level=6)"
     )
     assert markdown.markdown(str(heading)) == "<h6>Example heading</h6>"
+    
+    
+def test_heading_list():
+    """
+    Verifies that a Heading object properly 
+    initializes with a list of Inline objects. 
+    Also verifies that the repr string is 
+    properly formatted and that the markdown 
+    itself is properly rendered.
+    """
+    heading = Heading(["Example", " heading"], 1)
+    assert str(heading) == "# Example heading"
+    assert repr(heading) == (
+        r"Heading("
+        r"text=["
+            r"Inline("
+            r"text='Example', "
+            r"image=None, "
+            r"link=None, "
+            r"bold=False, "
+            r"italics=False, "
+            r"strikethrough=False, "
+            r"code=False"
+            r"), "
+            r"Inline("
+            r"text=' heading', "
+            r"image=None, "
+            r"link=None, "
+            r"bold=False, "
+            r"italics=False, "
+            r"strikethrough=False, "
+            r"code=False"
+            r")"
+        r"]"
+        r", level=1)"
+    )
+    assert markdown.markdown(str(heading)) == "<h1>Example heading</h1>"
+
+
+def test_heading_list_styling():
+    """
+    Verifies that a Heading object properly 
+    initializes with a list of Inline objects
+    to allow for custom styling. Also verifies 
+    that the repr string is properly formatted 
+    and that the markdown itself is properly 
+    rendered.
+    """
+    heading = Heading([Inline("Example", bold=True), " heading"], 1)
+    assert str(heading) == "# **Example** heading"
+    assert repr(heading) == (
+        r"Heading("
+        r"text=["
+            r"Inline("
+            r"text='Example', "
+            r"image=None, "
+            r"link=None, "
+            r"bold=True, "
+            r"italics=False, "
+            r"strikethrough=False, "
+            r"code=False"
+            r"), "
+            r"Inline("
+            r"text=' heading', "
+            r"image=None, "
+            r"link=None, "
+            r"bold=False, "
+            r"italics=False, "
+            r"strikethrough=False, "
+            r"code=False"
+            r")"
+        r"]"
+        r", level=1)"
+    )
+    assert markdown.markdown(str(heading)) == "<h1><strong>Example</strong> heading</h1>"
 
 
 def test_heading_str_level_zero_exception():
+    """
+    Verifies that a level provided below 1
+    causes an exception to be thrown.
+    """
     with pytest.raises(ValueError):
         Heading("Example heading", 0)
 
 
 def test_heading_str_level_seven_exception():
+    """
+    Verifies that a level provided above 6
+    causes an exception to be thrown.
+    """
     with pytest.raises(ValueError):
         Heading("Example heading", 7)
         
@@ -254,40 +337,51 @@ def test_heading_str_level_seven_exception():
 
 
 def test_heading_promote():
+    """
+    Verifies that heading can be correctly
+    promoted from lower level to higher level.
+    """
     heading = Heading("Example heading", 2)
     heading.promote()
     assert str(heading) == "# Example heading"
 
 
 def test_heading_promote_max():
+    """
+    Verifies that heading at maximum level (i.e., 1)
+    silently fails to promote.
+    """
     heading = Heading("Example heading", 1)
     heading.promote()
     assert str(heading) == "# Example heading"
 
 
 def test_heading_demote():
+    """
+    Verifies that heading can be correctly
+    demoted from higher level to lower level.
+    """
     heading = Heading("Example heading", 2)
     heading.demote()
     assert str(heading) == "### Example heading"
 
 
 def test_heading_demote_min():
+    """
+    Verifies that heading at minimum level (i.e., 6)
+    silently fails to demote.
+    """
     heading = Heading("Example heading", 6)
     heading.demote()
     assert str(heading) == "###### Example heading"
-
-
-def test_heading_list():
-    heading = Heading(["Example", " heading"], 1)
-    assert str(heading) == "# Example heading"
-
-
-def test_heading_list_styling():
-    heading = Heading([Inline("Example", bold=True), " heading"], 1)
-    assert str(heading) == "# **Example** heading"
     
 
 def test_repr_can_create_object():
+    """
+    Verifies that the __repr__ method can correctly
+    generate a string that can be used to create
+    an identical code block.
+    """
     heading = Heading("", 1)
     obj = eval(repr(heading))
     assert isinstance(obj, Heading) 

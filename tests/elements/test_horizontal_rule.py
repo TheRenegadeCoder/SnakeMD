ebee9fd2fc2ef4d42cff6c66c1843e222b47ef75
jrg94 <jeremy.grifski@gmail.com>
2023-05-04 14:28:22 -0400

    Added markdown to horizontal rule tests

--- tests/elements/test_horizontal_rule.py
+++ tests/elements/test_horizontal_rule.py
@@ -1,10 +1,18 @@
+import markdown
 from snakemd import HorizontalRule
+
+
+# Constructor tests
 
 
 def test_horizontal_rule():
     hr = HorizontalRule()
     assert str(hr) == "***"
     assert repr(hr) == "HorizontalRule()"
+    assert markdown.markdown(str(hr)) == "<hr />"
+
+
+# Method tests
 
 
 def test_repr_can_create_object():
